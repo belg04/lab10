c78db7dee7c3688c29a30a1c936eb9e99c961391
belg04 <113061405+belg04@users.noreply.github.com>
2023-09-23 11:59:53 +0200

    Добавьте файлы проекта.

---
+++ lab10/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MyApp //
+{
+    class Date
+    {
+        public DateTime now;
+
+        public Date(DateTime now)
+        {
+            this.now = now;
+        }
+    }
+    class Person : Date
+    {
+        public string name;
+        public DateTime hireDate;
+
+        public Person(string name, DateTime hireDate): base(hireDate) 
+        {
+            this.name = name;
+            this.hireDate = hireDate;
+        }
+
+        public int year() 
+        {
+            var zeroTime = new DateTime(1, 1, 1);
+            DateTime currentDate = DateTime.Now;
+            
+            return (zeroTime + (currentDate - hireDate)).Year - 1;
+        }
+
+    }
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Date d = new Date(DateTime.Now);
+            
+
+            Person p = new Person("Иван Иванович Петраков",new DateTime(2020,05,05));
+            
+            // Console.WriteLine(d.now.Subtract(p.hireDate)) ;
+
+            Console.WriteLine(p.year());
+        }
+        
+
+    }
+}
